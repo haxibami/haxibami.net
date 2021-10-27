ff3d53d1541b654d1a6bf8aa2a8f562ce2b46e47
haxibami <haxibami@gmail.com>
2021-10-27 17:37:15 +0900

    prettiered

--- haxibami.net/src/modules/grad_essay.ts
+++ haxibami.net/src/modules/grad_essay.ts
@@ -1,10 +1,10 @@
-import MdToHtml from './parser'
-import essay_starlight from '../articles/grad_essay.md'
+import MdToHtml from "./parser";
+import essay_starlight from "../articles/grad_essay.md";
 
 export async function getStaticProps() {
-  const contentHTML = await MdToHtml(essay_starlight)
+  const contentHTML = await MdToHtml(essay_starlight);
 
   return {
-    contentHTML
-  }
+    contentHTML,
+  };
 }
