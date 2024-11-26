fcd124992c42b3abeaa0f95daed7931ab63b215d
haxibami <me@haxibami.net>
2024-11-26 11:12:54 +0900

    perform build-time translation for mdx

--- src/lib/rehype-budoux.ts
+++ src/lib/rehype-budoux.ts
@@ -1,15 +1,19 @@
-import { h } from "hastscript";
 import { visit } from "unist-util-visit";
-// import { toHtml } from "hast-util-to-html";
-// import { fromHtml } from "hast-util-from-html";
-// import { loadDefaultJapaneseParser } from "budoux";
+import { toHtml } from "hast-util-to-html";
+import { fromHtml } from "hast-util-from-html";
+import { HTMLProcessingParser, jaModel } from "budoux";
+import { win } from "budoux/dist/win";
 
 import type { Element, Root } from "hast";
 import type { Plugin } from "unified";
 
-// const parser = loadDefaultJapaneseParser();
-
 const targetTags = ["p", "li"];
+
+const wbr = win.document.createElement("wbr");
+const parser = new HTMLProcessingParser(jaModel, {
+  className: "budoux",
+  separator: wbr,
+});
 
 function shouldTranslate(node: Element) {
   return (
@@ -22,22 +26,16 @@ function shouldTranslate(node: Element) {
 
 const rehypeBudoux: Plugin<[], Root> = () => {
   return (tree) => {
-    visit(tree, "element", (node) => {
+    visit(tree, "element", (node, index, parent) => {
+      if (typeof index !== "number") return;
       if (shouldTranslate(node)) {
-        // Method 1: innerHTML
-        node.children = [h("budoux-ja", node.children)];
-        // Method 2: replace node
-        // parent?.children.splice(index, 1, h("budoux-ja", [node]));
-        // Method 3: replace node with completely parsed node
-        // const newNode = fromHtml(
-        //   parser
-        //     .translateHTMLString(toHtml(node))
-        //     .replace(/\u200b/g, "<wbr />"),
-        //   {
-        //     fragment: true,
-        //   },
-        // ).children[0];
-        // newNode && parent?.children.splice(index, 1, newNode);
+        // Method 1: Runtime translation
+        // node.children = [h("budoux-ja", node.children)];
+        // Method 2: Build-time translation
+        const newNode = fromHtml(parser.translateHTMLString(toHtml(node)), {
+          fragment: true,
+        }).children[0];
+        newNode && parent?.children.splice(index, 1, newNode);
       }
     });
   };
